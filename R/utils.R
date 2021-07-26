bd72c06ab70eba0e5118cb8ec0a02fc442dfb358
Simon Opstrup Drue <simon.drue@clin.au.dk>
2021-07-26 16:38:55 +0200

    Added function sum_log_p

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+
+#' Calculate sum of probabilities in log-space
+#'
+#' @param lp_vec
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sum_log_p <- function(lp_vec) {
+  lp_max <- max(lp_vec)
+  return(lp_max + log(sum(exp(lp_vec - lp_max))))
+}
