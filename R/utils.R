677b334c78fca34e51b60fc6b2146e8c119e3a97
Simon Opstrup Drue <simon.drue@clin.au.dk>
2021-07-27 11:34:11 +0200

    Do not export sum_log_p

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,3 @@
-
-#' Calculate sum of probabilities in log-space.
-#'
-#' @param lp_vec numeric vector
-#'
-#' @return numeric value
-#' @export
-#'
-#' @examples
 sum_log_p <- function(lp_vec) {
   lp_max <- max(lp_vec)
   return(lp_max + log(sum(exp(lp_vec - lp_max))))
