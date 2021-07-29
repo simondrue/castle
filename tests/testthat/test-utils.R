7b2eaa137c09a6e23e6a1561927f6d3c59feea04
Simon Opstrup Drue <simon.drue@clin.au.dk>
2021-07-29 12:12:26 +0200

    Added tests to utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,24 +2,31 @@ test_that("sum_log_p", {
   log_p1 <- -1
   log_p2 <- -2
 
+  # Calculations
   expect_equal(sum_log_p(log_p1), log(exp(log_p1)))
   expect_equal(sum_log_p(c(log_p1, log_p2)), log(exp(log_p1) + exp(log_p2)))
+
+  # Avoid underflow (-Inf)
+  expect_equal(sum_log_p(c(-1000,-1000)), -999.30685)
+
+  # Output should be one number
   expect_equal(length(sum_log_p(c(log_p1, log_p2))), 1)
 })
 
-test_that("check_samples", {
-  # Empty sample
+test_that("check_input_samples", {
+  # Empty samples
   empty_sample <- data.frame(
     N_WT_only = c(0, 1, 0),
     N_M_only = c(0, 1, 0),
     N_d_neg = c(0, 1, 0),
     N_d_pos = c(0, 1, 0)
   )
+
   expect_error(
-    check_samples(
+    check_input_samples(
       samples = empty_sample
     ),
-    regexp = "total droplet count is 0"
+    regexp = "1,3.*total droplet count is 0"
   )
 
   # Incomplete data
@@ -30,9 +37,24 @@ test_that("check_samples", {
   )
 
   expect_error(
-    check_samples(
+    check_input_samples(
       samples = incomplete_sample
     ),
     regexp = "are missing from test_samples"
+  )
+
+  # No wild type negative
+  no_WT_sample <- data.frame(
+    N_WT_only = 1,
+    N_M_only = 0,
+    N_d_neg = 0,
+    N_d_pos = 1
+  )
+
+  expect_error(
+    check_input_samples(
+      samples = no_WT_sample
+    ),
+    regexp = "have no WT-negative droplets"
   )
 })
